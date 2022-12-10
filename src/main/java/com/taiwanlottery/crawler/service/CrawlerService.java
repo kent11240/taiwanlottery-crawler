e4b8d1341f7a33ec83475c3de0894cdf3a35a658
Kent Yu <kent1108@gmail.com>
2022-12-10 22:22:55 +0800

    trust all certs

--- src/main/java/com/taiwanlottery/crawler/service/CrawlerService.java
+++ src/main/java/com/taiwanlottery/crawler/service/CrawlerService.java
@@ -10,7 +10,14 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -100,9 +107,33 @@ public class CrawlerService {
         try {
             return Jsoup.connect(url)
                     .maxBodySize(Integer.MAX_VALUE)
+                    .sslSocketFactory(socketFactory())
                     .get();
         } catch (IOException e) {
             throw new CrawlerException(e);
+        }
+    }
+
+    private SSLSocketFactory socketFactory() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            return sslContext.getSocketFactory();
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Failed to create a SSL socket factory", e);
         }
     }
 }
